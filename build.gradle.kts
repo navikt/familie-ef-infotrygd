fa23cf85ffc1b0bee1d8f1ae29e63021bbf24203
Johan Blomgren <johan.blomgren@nav.no>
2020-12-15 09:00:07 +0100

    Må ha med validation i nye versjonen av spring for att token-security skal virke

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("io.micrometer:micrometer-core")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
