4e2bbf51aa80ca62d679ef5495f2f78639641ca0
Johan Blomgren <blommish@users.noreply.github.com>
2021-09-23 11:43:01 +0200

    Tar i bruk oracleoppset på nytt, og logger del av brukernavn og url

--- src/main/kotlin/no/nav/infotrygd/ef/rest/controller/PingController.kt
+++ src/main/kotlin/no/nav/infotrygd/ef/rest/controller/PingController.kt
@@ -1,6 +1,8 @@
 package no.nav.infotrygd.ef.rest.controller
 
 import no.nav.security.token.support.core.api.Unprotected
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,7 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/ping", produces = [MediaType.TEXT_PLAIN_VALUE])
 @Unprotected
-class PingController {
+class PingController(@Value("\${APP_DATASOURCE_USERNAME}") private val username: String,
+                     @Value("\${APP_DATASOURCE_USERNAME}") private val url: String) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    init {
+        logger.info("Username: ${username[0]}..${username.substringAfterLast("_")}")
+        logger.info("url: $url")
+    }
 
     @GetMapping
     fun ping() = "pong"
