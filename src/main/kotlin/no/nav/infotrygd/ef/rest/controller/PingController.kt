8939746ce8f71fd65051c20279a732397ec6a3cf
Johan Blomgren <blommish@users.noreply.github.com>
2021-09-23 12:13:49 +0200

    Sletter logging av brukernavn, nå virker replika i prod

--- src/main/kotlin/no/nav/infotrygd/ef/rest/controller/PingController.kt
+++ src/main/kotlin/no/nav/infotrygd/ef/rest/controller/PingController.kt
@@ -1,8 +1,6 @@
 package no.nav.infotrygd.ef.rest.controller
 
 import no.nav.security.token.support.core.api.Unprotected
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,15 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/ping", produces = [MediaType.TEXT_PLAIN_VALUE])
 @Unprotected
-class PingController(@Value("\${APP_DATASOURCE_USERNAME:username}") private val username: String,
-                     @Value("\${APP_DATASOURCE_URL:url}") private val url: String) {
-
-    private val logger = LoggerFactory.getLogger(javaClass)
-
-    init {
-        logger.info("Username: ${username.get(0)}..${username.substringAfterLast("_")}")
-        logger.info("url: $url")
-    }
+class PingController {
 
     @GetMapping
     fun ping() = "pong"
