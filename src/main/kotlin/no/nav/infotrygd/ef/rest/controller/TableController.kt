a34e2f3848691289878a39ff15080edfba69955a
Johan Blomgren <blommish@users.noreply.github.com>
2021-10-07 13:53:34 +0200

    Legger til count-check mot alle tabeller

--- src/main/kotlin/no/nav/infotrygd/ef/rest/controller/TableController.kt
+++ src/main/kotlin/no/nav/infotrygd/ef/rest/controller/TableController.kt
@@ -2,6 +2,7 @@ package no.nav.infotrygd.ef.rest.controller
 
 import no.nav.infotrygd.ef.integration.TableIntegrator
 import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.transaction.Transactional
@@ -9,9 +10,18 @@ import javax.transaction.Transactional
 @RestController
 @Unprotected
 @Transactional
-class TableController(private val tableIntegrator: TableIntegrator) {
+class TableController(private val tableIntegrator: TableIntegrator,
+                      private val jdbcTemplate: NamedParameterJdbcTemplate) {
+
     @GetMapping(path = ["/tables"])
     fun get(): Map<String, List<String>> {
         return tableIntegrator.tables
+    }
+
+    @GetMapping(path = ["/tables2"])
+    fun get2(): Map<String, Any?> {
+        return tableIntegrator.tables.keys.map {
+            it to jdbcTemplate.queryForObject("select count(*) from $it", emptyMap<String, Any>(), Int::class.java)
+        }.toMap()
     }
 }
