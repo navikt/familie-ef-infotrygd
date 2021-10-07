1c2f8b84dfb4d77dd51977ec07a4a5aaf42a58c5
Johan Blomgren <blommish@users.noreply.github.com>
2021-10-07 12:37:03 +0200

    logger brukernavn

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ export APP_DATASOURCE_USERNAME=$(cat "$APP_DATASOURCE_USERNAME_PATH" 2> /dev/nul
 export APP_DATASOURCE_PASSWORD=$(cat "$APP_DATASOURCE_PASSWORD_PATH" 2> /dev/null || echo $APP_DATASOURCE_PASSWORD)
 echo "- exported APP_DATASOURCE_USERNAME og APP_DATASOURCE_PASSWORD for infotrygd-enslig-forsoerger "
 
+echo "user $APP_DATASOURCE_USERNAME"
+
 if [ -z "$APP_DATASOURCE_USERNAME" ]
 then
   echo "username har ikke verdi"
