4c3002cf7594bf1b3181ee3defd651b191046bfb
ma10s <mattis.janitz@nav.no>
2022-05-24 16:42:53 +0200

    Formatering

--- src/test/kotlin/no/nav/familie/ef/infotrygd/service/PeriodeServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/infotrygd/service/PeriodeServiceTest.kt
@@ -3,13 +3,14 @@ package no.nav.familie.ef.infotrygd.service
 import io.mockk.every
 import io.mockk.mockk
 import no.nav.commons.foedselsnummer.FoedselsNr
-import no.nav.familie.ef.infotrygd.model.StønadType.*
+import no.nav.familie.ef.infotrygd.model.StønadType.BARNETILSYN
+import no.nav.familie.ef.infotrygd.model.StønadType.OVERGANGSSTØNAD
 import no.nav.familie.ef.infotrygd.repository.PeriodeRepository
 import no.nav.familie.ef.infotrygd.rest.api.InfotrygdEndringKode
 import no.nav.familie.ef.infotrygd.rest.api.InfotrygdSakstype
 import no.nav.familie.ef.infotrygd.rest.api.Periode
 import no.nav.familie.ef.infotrygd.rest.api.PeriodeRequest
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -22,13 +23,13 @@ internal class PeriodeServiceTest {
     @Test
     fun `Skal legge til barn på barnetilsyn - andre skal være uendret`() {
         val request = PeriodeRequest(
-            personIdenter = setOf(FoedselsNr("01015450572")),
-            stønadstyper = setOf(BARNETILSYN, OVERGANGSSTØNAD)
+                personIdenter = setOf(FoedselsNr("01015450572")),
+                stønadstyper = setOf(BARNETILSYN, OVERGANGSSTØNAD)
         )
         val uendretPeriode = lagPeriode()
         every { periodeRepository.hentPerioder(any()) } returns listOf(
-            Pair(BARNETILSYN, lagPeriode(vedtakId=35L)),
-            Pair(OVERGANGSSTØNAD, uendretPeriode)
+                Pair(BARNETILSYN, lagPeriode(vedtakId = 35L)),
+                Pair(OVERGANGSSTØNAD, uendretPeriode)
         )
         every { periodeRepository.hentBarnForPerioder(any()) } returns mapOf(35L to listOf("123"))
 
@@ -41,13 +42,13 @@ internal class PeriodeServiceTest {
     @Test
     fun `Ingen barnetilsynbarn funnet - skal ikke feile hvis det ikke finnes barn`() {
         val request = PeriodeRequest(
-            personIdenter = setOf(FoedselsNr("01015450572")),
-            stønadstyper = setOf(BARNETILSYN, OVERGANGSSTØNAD)
+                personIdenter = setOf(FoedselsNr("01015450572")),
+                stønadstyper = setOf(BARNETILSYN, OVERGANGSSTØNAD)
         )
         val uendretPeriode = lagPeriode()
         every { periodeRepository.hentPerioder(any()) } returns listOf(
-            Pair(BARNETILSYN, lagPeriode(vedtakId=35L)),
-            Pair(OVERGANGSSTØNAD, uendretPeriode)
+                Pair(BARNETILSYN, lagPeriode(vedtakId = 35L)),
+                Pair(OVERGANGSSTØNAD, uendretPeriode)
         )
         every { periodeRepository.hentBarnForPerioder(any()) } returns emptyMap()
 
@@ -58,28 +59,28 @@ internal class PeriodeServiceTest {
     }
 
     private fun lagPeriode(vedtakId: Long = 1) = Periode(
-        personIdent = "123",
-        sakstype =InfotrygdSakstype.SØKNAD,
-        kode =InfotrygdEndringKode.FØRSTEGANGSVEDTAK,
-        kodeOvergangsstønad = null,
-        aktivitetstype = null,
-        brukerId = "",
-        stønadId = 0,
-        vedtakId = vedtakId,
-        vedtakstidspunkt = LocalDateTime.MIN,
-        stønadBeløp = 0,
-        engangsbeløp = 0,
-        inntektsgrunnlag = 0,
-        inntektsreduksjon = 0,
-        samordningsfradrag = 0,
-        utgifterBarnetilsyn = 0,
-        beløp = 0,
-        månedsbeløp = 0,
-        startDato = LocalDate.MIN,
-        stønadFom = LocalDate.MIN,
-        stønadTom = LocalDate.MAX,
-        opphørsdato = null,
-        barnIdenter = emptyList()
+            personIdent = "123",
+            sakstype = InfotrygdSakstype.SØKNAD,
+            kode = InfotrygdEndringKode.FØRSTEGANGSVEDTAK,
+            kodeOvergangsstønad = null,
+            aktivitetstype = null,
+            brukerId = "",
+            stønadId = 0,
+            vedtakId = vedtakId,
+            vedtakstidspunkt = LocalDateTime.MIN,
+            stønadBeløp = 0,
+            engangsbeløp = 0,
+            inntektsgrunnlag = 0,
+            inntektsreduksjon = 0,
+            samordningsfradrag = 0,
+            utgifterBarnetilsyn = 0,
+            beløp = 0,
+            månedsbeløp = 0,
+            startDato = LocalDate.MIN,
+            stønadFom = LocalDate.MIN,
+            stønadTom = LocalDate.MAX,
+            opphørsdato = null,
+            barnIdenter = emptyList()
     )
 
 
