f725880d8814fabdebea3313b3844d940548e491
throndi <56258085+throndi@users.noreply.github.com>
2024-01-11 10:19:57 +0100

    Fjerner info flagg

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 
 group = "no.nav"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 configurations {
     compileOnly {
